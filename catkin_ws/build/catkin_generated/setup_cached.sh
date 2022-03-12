29576f78e4699810a1d0fd6f58578f71ad1f6703
Aloksha2001 <ali.elmorsy@epfl.ch>
2022-03-12 19:28:28 +0100

    push vision

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/aloksha/Desktop/HD_workspace/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/aloksha/Desktop/HD_workspace/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/aloksha/Desktop/HD_workspace/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/aloksha/Desktop/HD_workspace/catkin_ws/src:$ROS_PACKAGE_PATH"

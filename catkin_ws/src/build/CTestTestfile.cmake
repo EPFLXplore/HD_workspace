e82b220390b9d9926ba9c0270778421648c90b09
Benjamin Colety <benjamin.colety@epfl.ch>
2022-04-10 23:43:20 +0200

    got rid of ben's build

--- catkin_ws/src/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ben18/xplore/HD_workspace/catkin_ws/src
-# Build directory: /home/ben18/xplore/HD_workspace/catkin_ws/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("detection_manager")

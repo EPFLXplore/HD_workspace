9877642496574dc84e7d390031ae5fb90d0a2a8e
Aloksha2001 <ali.elmorsy@epfl.ch>
2022-05-16 22:22:51 +0200

    added commander test node

--- catkin_ws/src/vision_no_ros/src/vision_no_ros.cpp
+++ catkin_ws/src/vision_no_ros/src/vision_no_ros.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <ros/ros.h>
 
+#include <iostream>
+#include "std_msgs/Int16.h"
+
 //includes for my headers
 #include <vision_no_ros/cntrl_pnl.h> //included in  object_refresh
 #include <vision_no_ros/cv-helpers.hpp>
@@ -15,23 +18,40 @@
 using namespace std;
 using namespace cv;
 
-cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_7X7_250);
+static bool show_input_image(0);
+static bool show_output_image(0);
+#define SAMPLES 30
 
-void fsm_callback(const std_msgs::String::ConstPtr& msg){
-ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+////////////////////// vectors required for AR tag detection ///////////////////////////////////
+cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_7X7_250);
+static cv::Mat cameraMatrix ;
+static cv::Mat distCoeffs ;
+static vector<int> ids;
+static vector<vector<Point2f> > corners;
+static std::vector<cv::Vec3d> rvecs, tvecs;
+
+void fsm_callback(const std_msgs::Int16& msg){
+  // this should comtain the ros ok whuile loop but with another condition
+
+        cout << msg.data << endl;
 }
  
 
 int main(int argc, char **argv) try {   
     
+    //////////// ROS node initialisation ////////////////
     ros::init(argc, argv, "detected_elements_publisher");
     ros::NodeHandle n;
     ros::Publisher pub = n.advertise<vision_no_ros::object_list>("detected_elements", 1);
     ros::Subscriber sub = n.subscribe("vision_FSM", 1000, fsm_callback);
-    
+    //ros::spin();//this is needed for the calbacks to be actually alled its an infinite loop...
+
+    //////////// control panel initialisation ////////////
     cntrl_pnl::ControlPanel my_panel;
     cntrl_pnl::setup_control_panel(my_panel);
-    // Declare RealSense pipeline, encapsulating the actual device and sensors
+    
+    ////////// RealSense pipeline initialisation /////////
     rs2::pipeline pipe;
     //setup custom streaming configuration 
     /*
@@ -41,95 +61,91 @@ int main(int argc, char **argv) try {
     pipe.start(cfg);
     */
     pipe.start(); // Start streaming with default recommended configuration//maybe should try to optimze the start parameters for bandwidth gains and other stuff when integratingg
-
     rs2::align align_to_color(RS2_STREAM_COLOR);//expensive keep it outside the loop
-   
-    const auto window_name = "Display Image";
-    namedWindow(window_name, WINDOW_AUTOSIZE);
 
-    while (waitKey(0) > -1 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0) //this was waitkey(1) <0
-    {
+
+    while (ros::ok()){ // need the loop to keep getting new frames replace the condition with something from the commands i get from the fsm maybe I should add an idle state to this{
+        ///////////////get new depth and color frames and align them///////////////////////////
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
-        
-        
-        data = align_to_color.process(data);
-
-
+        data = align_to_color.process(data); //for aligning the depth and color frames
         rs2::frame color = data.get_color_frame();
         rs2::depth_frame depth =data.get_depth_frame();
        
 
-        static cv::Mat cameraMatrix ;
-        static cv::Mat distCoeffs ;
+        ///////////////// AR tag detection and camera calibration /////////////////////////
         rs2_intrinsics intrinsics = get_field_of_view(pipe,cameraMatrix,distCoeffs); //function to get the camera intrinsics and copy them into the right matrices
-        
-   
-        // vectors required for AR tag detection
-        static vector<int> ids;
-        static vector<vector<Point2f> > corners;
-        static std::vector<cv::Vec3d> rvecs, tvecs;
-
         cv::Mat image = frame_to_mat(color);  //using the cv helpers to convert an rs2 frame to a cv mat
-
         cv::aruco::detectMarkers(image,dictionary,corners,ids);
+        
+        
+        
         if (ids.size()>0){
-            cv::Mat output_image=image.clone();
-            cv::aruco::drawDetectedMarkers(output_image,corners,ids);
-            cv::aruco::estimatePoseSingleMarkers(corners, 0.044, cameraMatrix, distCoeffs, rvecs, tvecs);// dont forget to modify the ar tag size!! //this function might become obsolete
+            
+            cv::aruco::estimatePoseSingleMarkers(corners, 0.044, cameraMatrix, distCoeffs, rvecs, tvecs);// dont forget to modify the ar tag size!! //this function might become obsolete this will need to be added inside the ifs because the ar tag size changes
             
             uint command=1;// test variable , replaces the topic I should be subscribed to to know which on=bject to manipulate
-            static int samples=30;
             static int active_sample=0;
-            for(int i=0; i<ids.size(); i++){ //why do I need this ? this shpuld be done for each frame no need for the ids.size
-                //cv::aruco::drawAxis(output_image, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1); removed his for the jetson (uses a version thats not compatible with this)
-                                
-                //start refreshing the objects
-                vision_no_ros::object_list objects;//decalre objects list
-                
-                if (command==0 or command==1){
-                    //declare object and refresh it
-                    vision_no_ros::panel_object main_switch;
-                    refresh_object(main_switch,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switchMain,depth,corners,intrinsics,samples);//need to make a function that gets the rvecs and tvecs for the ar tag with id hard coded
-                    //push back the object to the list to be published
-                    if (active_sample==samples) objects.detected_objects.push_back(main_switch);
-                }
-                if (command==0 or command==2){
-                    vision_no_ros::panel_object switch_1;
-                    refresh_object(switch_1,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switch1,depth,corners,intrinsics,samples);
-                    if (active_sample==samples) objects.detected_objects.push_back(switch_1);
-                }
-
-                if (command==0 or command==3){
-                    vision_no_ros::panel_object switch_2;
-                    refresh_object(switch_2,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switch2,depth,corners,intrinsics,samples);
-                    if (active_sample==samples) objects.detected_objects.push_back(switch_2);
-                }
-
-                if (command==0 or command==4){
-                    vision_no_ros::panel_object switch_3;
-                    refresh_object(switch_3,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switch3,depth,corners,intrinsics,samples);
-                    if (active_sample==samples) objects.detected_objects.push_back(switch_3);
-                }
-                
-                if (active_sample < samples ){
-                    ++active_sample;
-                }else {
-                    active_sample=0;
-                }
-
-                
-                //publish the list               
-                pub.publish(objects);
-                ros::spinOnce();
+            ///////////////////////////////////////////////// start refreshing the objects ///////////////////////////////////          
+            vision_no_ros::object_list objects;//decalre objects list
             
+            if (command==0 or command==1){
+                //declare object and refresh it
+                vision_no_ros::panel_object main_switch;
+                refresh_object(main_switch,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switchMain,depth,corners,intrinsics,SAMPLES);//need to make a function that gets the rvecs and tvecs for the ar tag with id hard coded
+                //push back the object to the list to be published
+                if (active_sample==SAMPLES) objects.detected_objects.push_back(main_switch);
             }
-            imshow(window_name, output_image);
+            if (command==0 or command==2){
+                vision_no_ros::panel_object switch_1;
+                refresh_object(switch_1,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switch1,depth,corners,intrinsics,SAMPLES);
+                if (active_sample==SAMPLES) objects.detected_objects.push_back(switch_1);
+            }
+
+            if (command==0 or command==3){
+                vision_no_ros::panel_object switch_2;
+                refresh_object(switch_2,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switch2,depth,corners,intrinsics,SAMPLES);
+                if (active_sample==SAMPLES) objects.detected_objects.push_back(switch_2);
+            }
+
+            if (command==0 or command==4){
+                vision_no_ros::panel_object switch_3;
+                refresh_object(switch_3,ids,rvecs,tvecs,my_panel.panelA.artg1,my_panel.panelA.switch3,depth,corners,intrinsics,SAMPLES);
+                if (active_sample==SAMPLES) objects.detected_objects.push_back(switch_3);
+            }
+            /////////////////////////////////////////////// end object referesh /////////////////////////////////////////////////
+
+
+
+            if (active_sample < SAMPLES ){
+                ++active_sample;
+            }else {
+                active_sample=0;
+            }
+                
+            //publish the list               
+            pub.publish(objects);
+            ros::spinOnce();
+        
+            if (show_output_image){
+                cv::Mat output_image=image.clone();
+                cv::aruco::drawDetectedMarkers(output_image,corners,ids);
+                //for(int i=0; i<ids.size(); i++){ 
+                //  cv::aruco::drawAxis(output_image, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1); removed his for the jetson (uses a version thats not compatible with this)
+                //} 
+                imshow("output image", output_image);
+                waitKey(1);
+            }
+        }else {
+            // no ar tags are visible call the blind functions
         }
-        imshow("input feed",image);
+        if(show_input_image){
+            imshow("input feed",image);
+            waitKey(1);
+        }
     }
-    // destroyAllWindows(); // doesnt do much here
     return EXIT_SUCCESS;
 }
+
 catch (const rs2::error & e)
 {
     std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
